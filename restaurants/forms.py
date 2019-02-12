86208ed7df30eec10d03b3e70c9481aa6178dcc4
abdullah <anabanmi@hotmail.com>
2019-02-12 10:16:06 +0300

    added create resturant button & adjust time input type.

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Restaurant
 
 class RestaurantForm(forms.ModelForm):
-    class Meta:
-        model = Restaurant
-        fields = '__all__'
+	class Meta:
+		model = Restaurant
+		fields = '__all__'
+		
+		widgets = {
+			'opening_time': forms.TimeInput(attrs = {'type': 'time'}), 
+			'closing_time': forms.TimeInput(attrs = {'type': 'time'})
+		}
